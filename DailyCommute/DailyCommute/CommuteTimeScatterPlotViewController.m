50ddc3df7077aa11204217efe2e792619e994b96
Harlan Haskins <harlan@valleyrocket.com>
2012-09-09 23:58:03 -0400

    Fiddled with graph settings.
    Working to make X-Axis permanently visible.

--- DailyCommute/DailyCommute/CommuteTimeScatterPlotViewController.m
+++ DailyCommute/DailyCommute/CommuteTimeScatterPlotViewController.m
@@ -259,7 +259,10 @@
     // Edit the sort key as appropriate.
     NSSortDescriptor *sortDescriptor = [[NSSortDescriptor alloc] initWithKey:@"startTime" ascending:YES];
     [fetchRequest setSortDescriptors:@[sortDescriptor]];
+<<<<<<< HEAD
     //<<<<<<< HEAD
+=======
+>>>>>>> Fiddled with graph settings.
     
     NSArray *array = [self.managedObjectContext executeFetchRequest:fetchRequest error:&error];
     for (Route *route in array) {
@@ -272,6 +275,7 @@
             NSString* dateString = [df stringFromDate:route.endTime];
             [self.dateStringArray addObject:dateString];
         }
+<<<<<<< HEAD
         //=======
         //NSLog(@"%@", fetchRequest);
         NSArray *array = [self.managedObjectContext executeFetchRequest:fetchRequest error:&error];
@@ -310,6 +314,40 @@
             abort();
         }
     }
+=======
+    //NSLog(@"%@", fetchRequest);
+    NSArray *array = [self.managedObjectContext executeFetchRequest:fetchRequest error:&error];
+    for (Route *route in array) {
+        NSLog(@"%@", route.toCommute);
+        NSLog(@"%@", route);
+        [self.routeArray addObject:route];
+        NSNumber *time = [NSNumber numberWithInteger:[route.endTime timeIntervalSinceDate:route.startTime]];
+        [self.timeIntervalArray addObject:time];
+        NSDateFormatter *df = [[NSDateFormatter alloc] init];
+        df.dateFormat = @"MM/dd/yyyy";
+        NSString* dateString = [df stringFromDate:route.endTime];
+        [self.dateStringArray addObject:dateString];
+    }
+    
+    NSLog(@"Time Intervals: %@", self.timeIntervalArray);
+    NSLog(@"Routes: %@", self.routeArray);
+    NSLog(@"Date Strings: %@", self.dateStringArray);
+<<<<<<< HEAD
+    error = nil;
+    // Edit the section name key path and cache name if appropriate.
+    // nil for section name key path means "no sections".
+    
+    NSFetchedResultsController *aFetchedResultsController = [[NSFetchedResultsController alloc] initWithFetchRequest:fetchRequest managedObjectContext:self.managedObjectContext sectionNameKeyPath:nil cacheName:nil];
+    aFetchedResultsController.delegate = self;
+    self.fetchedResultsController = aFetchedResultsController;
+    
+	if (![self.fetchedResultsController performFetch:&error]) {
+        // Replace this implementation with code to handle the error appropriately.
+        // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+	    NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
+	    abort();
+	}
+>>>>>>> Fiddled with graph settings.
 }
 
 @end
